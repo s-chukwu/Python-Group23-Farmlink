ba1f48af612ba4a978b8d44138266c3e796286c6
Magnificat Marie Augusta Umutesi <umutesimagnificat@gmail.com>
2026-03-26 18:51:55 +0200

    add logic for signup and login

--- farmlink.py
+++ farmlink.py
@@ -1,3 +1,6 @@
+import email
+import getpass
+import re
 import sqlite3
 
 def init_db():
@@ -51,3 +54,88 @@ def init_db():
     
     conn.commit()
     conn.close()
+# Authentication
+
+# signup
+def signup():
+    conn = sqlite3.connect("farmlink.db")
+    cursor = conn.cursor()
+    
+    try:
+        name = input("Enter your name: ")
+
+        #check if there is an existing user with the inserted email
+        while True:
+            email = input("Enter your email: ")
+            if not userExists(conn, email):
+                break
+            print('Account already exists, please enter another one or login')
+
+        # Obscure password while entering it
+        password = getpass.getpass("Enter your password: ")
+
+        # get valid role
+        role=get_valid_role()
+        cursor.execute('INSERT INTO users (name, email, password, role) values (?, ?, ?, ?)', (name, email, password, role))
+        conn.commit()
+    except sqlite3.IntegrityError:
+        print('Database error occurred, please try again')
+    except Exception as e:
+        print('Unknown error, please try again')
+    finally:
+         conn.close()
+
+def userExists(conn, email):
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM users WHERE email = ?', (email,))
+    user = cursor.fetchone()
+    if user is None:
+        return False
+    else:
+        return True
+
+def is_valid_email(email):
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    return bool(re.match(pattern, email))
+
+def get_valid_role():
+    while True:
+        try:
+            role_choice = int(input("Enter a number that matches who you are registering as: \n 1. Farmer \n 2. Buyer"))
+            if role_choice == 1:
+               return "Farmer"
+            elif role_choice == 2:
+               return "Buyer"
+            else:
+             print("Please enter a valid choice")
+        except ValueError:
+            print("Enter a number please! :)")
+
+# Login
+def login():
+    conn = sqlite3.connect("farmlink.db")
+    cursor = conn.cursor()
+    max_attempts = 4
+    try:
+        for attempt in range(max_attempts):
+            email = input("Enter your email: ")
+            password = getpass.getpass("Enter your password: ")
+
+            cursor.execute('SELECT * FROM users WHERE (email,password) = (?,?)', (email,password))
+            user = cursor.fetchone()
+            if user is not None:
+                print('Welcome, {}!'.format(user[0]))
+                ## Call the appropriate method for the given role
+                return
+            else:
+                remaining_attempts = max_attempts - attempt
+                print(f'Please enter a valid email and password, you are left with {remaining_attempts}')
+
+        print('You have reached the maximum number of attempts to login, try again later.')
+        return None
+
+    except Exception as e:
+        print('Error occurred while logging in, please try again later')
+    finally:
+        conn.close()
+
